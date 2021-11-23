82931b1ec46f4cf80e0c2854d2c17b5bfcdb4733
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2021-11-23 23:13:26 +0530

    chore: inits and changing fonts

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
+import '@fontsource/fira-mono';
+import '@fontsource/allerta-stencil';
+
 import React from 'react';
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
   VStack,
-  Code,
   Grid,
-  theme,
+  Container,
+  Stack,
+  Heading,
+  Text,
 } from '@chakra-ui/react';
+
+import theme from './theme';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
 
 function App() {
   return (
@@ -19,19 +23,22 @@ function App() {
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
+            <Container>
+              <Stack>
+                <Heading>The spectacle before us was indeed sublime.</Heading>
+                <Text>
+                  Apparently we had reached a great height in the atmosphere,
+                  for the sky was a dead black, and the stars had ceased to
+                  twinkle. By the same illusion which lifts the horizon of the
+                  sea to the level of the spectator on a hillside, the sable
+                  cloud beneath was dished out, and the car seemed to float in
+                  the middle of an immense dark sphere, whose upper half was
+                  strewn with silver. Looking down into the dark gulf below, I
+                  could see a ruddy light streaming through a rift in the
+                  clouds.
+                </Text>
+              </Stack>
+            </Container>
           </VStack>
         </Grid>
       </Box>
---
+++ client/src/theme.js
@@ -0,0 +1,11 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  fonts: {
+    heading: 'Allerta Stencil',
+    body: 'Fira Mono',
+    // body: 'Bellefair',
+  },
+});
+
+export default theme;
